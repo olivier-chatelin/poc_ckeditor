12617fce4dc44899ea5095f5ef03f7771e643993
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-07-05 11:50:18 +0200

    poc

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Blog;
+use App\Form\BlogType;
+use App\Repository\BlogRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $blog = new Blog();
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($blog);
+            $entityManager->flush();
+        }
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/show", name="show")
+     */
+    public function show(BlogRepository $blogRepository): Response
+    {
+        $blogs = $blogRepository->findAll();
+
+        return $this->render('home/show.html.twig', [
+            'blogs' => $blogs,
+        ]);
+    }
+}
---
+++ src/Form/BlogType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Blog;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description', CKEditorType::class,[
+                'config' => ['uiColor' => '#8C08F5',],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Blog::class,
+        ]);
+    }
+}
